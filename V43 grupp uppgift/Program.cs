e0eed6cb9ed1333ac555fb59f923b287e2674a63
Amanda Kilström <Amanda.kilstrom@hotmail.com>
2024-10-24 14:04:57 +0200

    Ändringar i menyn#

--- V43 grupp uppgift/Program.cs
+++ V43 grupp uppgift/Program.cs
@@ -4,29 +4,35 @@ namespace V43_grupp_uppgift
 {
     internal class Program
     {
-        List<Användare> användare = new List<Användare> ();
+        public static List<User> användare = new List<User> ();
         static void Main(string[] args)
         {
 
             User admin = new User(true); 
             User user = new User(false);
+            användare.Add(admin);
+            användare.Add(user);
             Console.WriteLine("Välkommen att logga in");
             Console.Write("Användarnamn: ");
             string användarnamn = Console.ReadLine();
             Console.Write("Lössenord: ");
             string lösenord = Console.ReadLine();
-            if (admin.IsAdmin == true)
-            {
-                Admin();
-            }
-            else
-            {
-                Användare();
-            }
-
             
-
-
+            
+            foreach (User a in användare)
+            {
+                if (a.IsAdmin)
+                {
+                    if (admin.IsAdmin == true)
+                    {
+                        Admin();
+                    }
+                    else
+                    {
+                        Användare();
+                    }
+                }
+            }
         }
 
         static void Användare()
