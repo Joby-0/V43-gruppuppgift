c66ed096805ad70d1eb72e76df92d16972949535
Joby-01 <Johan@PROBOOK>
2024-10-24 13:02:56 +0200

    Add project files.

---
+++ V43 grupp uppgift/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace V43_grupp_uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Användare()
+        {
+
+        }
+        static void Admin()
+        {
+
+        }
+
+    }
+}
